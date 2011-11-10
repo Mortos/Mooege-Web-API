67b47643676d60720cfc090671d381079a85836f
Ste Rug <st3fan_01@hotmail.com>
2011-11-10 14:32:24 +0100

    rename file name

---
+++ registration.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form enctype="multipart/form-data" method="post" action="encrypt.php"  >
+Email:<input type="text" name="email" ><br>
+Password:<input type="password" name="password"><br>
+<input name="isReg" type="submit" value="Register">
+</form>
+</body>
+</html>
